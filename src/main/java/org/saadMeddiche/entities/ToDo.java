9d0591f2a564ba0e13aed2de5146b2505d28a6a1
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-08-07 16:51:31 +0100

    Add Lombok annotations to ToDo class for improved code simplicity

--- src/main/java/org/saadMeddiche/entities/ToDo.java
+++ src/main/java/org/saadMeddiche/entities/ToDo.java
@@ -1,5 +1,10 @@
 package org.saadMeddiche.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder @NoArgsConstructor @AllArgsConstructor
 public class ToDo {
     public Long id;
     public String title;
