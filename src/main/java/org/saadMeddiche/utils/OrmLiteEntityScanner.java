26f1265f5f91d04967cecb7953256838b8043d58
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-08-08 17:12:05 +0100

    Add OrmLiteEntityScanner class to find ORM entities and include classgraph dependency in pom.xml

---
+++ src/main/java/org/saadMeddiche/utils/OrmLiteEntityScanner.java
@@ -0,0 +1,27 @@
+package org.saadMeddiche.utils;
+
+import io.github.classgraph.ClassGraph;
+import io.github.classgraph.ClassInfo;
+import io.github.classgraph.ScanResult;
+import com.j256.ormlite.table.DatabaseTable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class OrmLiteEntityScanner {
+    public static List<Class<?>> findEntities(String packageName) {
+        List<Class<?>> entities = new ArrayList<>();
+
+        try (ScanResult scanResult = new ClassGraph()
+                .enableAllInfo()
+                .acceptPackages(packageName)
+                .scan()) {
+
+            for (ClassInfo classInfo : scanResult.getClassesWithAnnotation(DatabaseTable.class)) {
+                entities.add(classInfo.loadClass());
+            }
+
+        }
+        return entities;
+    }
+}
