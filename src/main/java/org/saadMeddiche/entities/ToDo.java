dcc0905cc0ec5973f54963690279ed6043660830
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-08-08 16:25:16 +0100

    Add User entity and update ToDo entity for ORM support

--- src/main/java/org/saadMeddiche/entities/ToDo.java
+++ src/main/java/org/saadMeddiche/entities/ToDo.java
@@ -1,12 +1,25 @@
 package org.saadMeddiche.entities;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 
+@DatabaseTable(tableName = "todo")
 @Builder @NoArgsConstructor @AllArgsConstructor
 public class ToDo {
+
+    @DatabaseField(generatedId = true)
     public Long id;
+
+    @DatabaseField(canBeNull = false)
     public String title;
+
+    @DatabaseField()
     public String description;
+
+    @DatabaseField(foreign = true, foreignAutoRefresh = true)
+    private User user;
+
 }
---
+++ src/main/java/org/saadMeddiche/entities/User.java
@@ -0,0 +1,32 @@
+package org.saadMeddiche.entities;
+
+import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.field.ForeignCollectionField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "user")
+public class User {
+
+    @DatabaseField(generatedId = true)
+    public Long id;
+
+    @DatabaseField(canBeNull = false)
+    public String firstName;
+
+    @DatabaseField(canBeNull = false)
+    public String lastName;
+
+    @DatabaseField(canBeNull = false, unique = true)
+    public String username;
+
+    @DatabaseField(canBeNull = false, unique = true)
+    public String email;
+
+    @DatabaseField(canBeNull = false)
+    public String password;
+
+    @ForeignCollectionField
+    private ForeignCollection<ToDo> toDos;
+
+}
