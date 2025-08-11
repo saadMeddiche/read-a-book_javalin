145023123130b73bdbda526f89015470c3e5d7e8
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-08-11 15:30:24 +0100

    refactor: centralize method of creating connection source.

--- src/main/java/org/saadMeddiche/repositories/TablesInitializer.java
+++ src/main/java/org/saadMeddiche/repositories/TablesInitializer.java
@@ -1,9 +1,9 @@
 package org.saadMeddiche.repositories;
 
-import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 import lombok.extern.slf4j.Slf4j;
-import org.saadMeddiche.configurations.DataBaseConfiguration;
+import org.saadMeddiche.utils.DatabaseConnectionProvider;
 import org.saadMeddiche.utils.OrmLiteEntityScanner;
 
 import java.sql.SQLException;
@@ -18,7 +18,7 @@ public class TablesInitializer {
         log.info("Initializing database tables for package: {}", entityPackage);
 
         log.info("Creating connection source...");
-        JdbcPooledConnectionSource connectionSource = createConnectionSource();
+        ConnectionSource connectionSource = DatabaseConnectionProvider.getConnectionSource();
 
         log.info("Scanning for entities in package: {}", entityPackage);
         List<Class<?>> entities = OrmLiteEntityScanner.findEntities(entityPackage);
@@ -36,22 +36,11 @@ public class TablesInitializer {
 
     }
 
-    private JdbcPooledConnectionSource createConnectionSource() throws SQLException {
-
-        DataBaseConfiguration config = DataBaseConfiguration.INSTANCE;
-
-        JdbcPooledConnectionSource connectionSource = new JdbcPooledConnectionSource(config.JDBC_URL);
-        connectionSource.setUsername(config.JDBC_USER);
-        connectionSource.setPassword(config.JDBC_PASSWORD);
-
-        return connectionSource;
-
-    }
-
     // TODO: Implement filtering logic to skip entities that already have a table
-    private List<Class<?>> filterEntitiesThatAlreadyHaveTable(List<Class<?>> entities, JdbcPooledConnectionSource connectionSource) throws SQLException {
+    private List<Class<?>> filterEntitiesThatAlreadyHaveTable(List<Class<?>> entities, ConnectionSource connectionSource) throws SQLException {
         List<Class<?>> filteredEntities = new ArrayList<>();
-        for (Class<?> entity : entities) {}
+        for (Class<?> entity : entities) {
+        }
         return filteredEntities;
     }
 
--- src/main/java/org/saadMeddiche/utils/DatabaseConnectionProvider.java
+++ src/main/java/org/saadMeddiche/utils/DatabaseConnectionProvider.java
@@ -1,5 +1,7 @@
 package org.saadMeddiche.utils;
 
+import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
+import com.j256.ormlite.support.ConnectionSource;
 import org.saadMeddiche.configurations.DataBaseConfiguration;
 
 import java.sql.Connection;
@@ -12,6 +14,13 @@ public class DatabaseConnectionProvider {
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(connectionConfiguration.JDBC_URL, connectionConfiguration.JDBC_USER, connectionConfiguration.JDBC_PASSWORD);
+    }
+
+    public static ConnectionSource getConnectionSource() throws SQLException {
+        JdbcPooledConnectionSource connectionSource = new JdbcPooledConnectionSource(connectionConfiguration.JDBC_URL);
+        connectionSource.setUsername(connectionConfiguration.JDBC_USER);
+        connectionSource.setPassword(connectionConfiguration.JDBC_PASSWORD);
+        return connectionSource;
     }
 
 }
