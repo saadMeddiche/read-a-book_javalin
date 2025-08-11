84e4f881086e3236f96a0bf656f18455377c27aa
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-08-11 15:26:26 +0100

    fix: add try-catch to prevent the app from crashing when tables already exits

--- src/main/java/org/saadMeddiche/Main.java
+++ src/main/java/org/saadMeddiche/Main.java
@@ -1,16 +1,22 @@
 package org.saadMeddiche;
 
 import io.javalin.Javalin;
+import lombok.extern.slf4j.Slf4j;
 import org.saadMeddiche.controllers.ToDoController;
 import org.saadMeddiche.repositories.TablesInitializer;
 
 import java.sql.SQLException;
 
+@Slf4j
 public class Main {
 
     public static void main(String[] args) throws SQLException {
         Main main = new Main();
-        main.setupDatabaseTable();
+        try {
+            main.setupDatabaseTable();
+        }catch (Exception e){
+            log.error("This is only a temporary fix to avoid the application from crashing when the database table already exists. " , e);
+        }
         main.setupEndpoints();
     }
 
