59eac8207467bde0d2fd9f50c514f3eeed62d3dd
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-08-12 16:17:16 +0100

    feat: enhance UserInitializer to create a user during initialization

--- src/main/java/org/saadMeddiche/initializers/impl/UserInitializer.java
+++ src/main/java/org/saadMeddiche/initializers/impl/UserInitializer.java
@@ -2,13 +2,23 @@ package org.saadMeddiche.initializers.impl;
 
 import lombok.extern.slf4j.Slf4j;
 import org.saadMeddiche.initializers.Initializable;
+import org.saadMeddiche.repositories.UserRepository;
+import org.saadMeddiche.repositories.impl.UserSimpleRepository;
+import org.saadMeddiche.requests.UserCreateRequest;
 
 @Slf4j
 public class UserInitializer implements Initializable {
 
+    private final UserRepository userRepository = UserSimpleRepository.INSTANCE;
+
     @Override
     public void initialize() {
-        log.info("Initializing User");
+
+        UserCreateRequest userCreateRequest = new UserCreateRequest("Bob","Callahan","TheBob","bobCallahan@gmail.com","password123");
+        log.info("Initializing User with request: {}", userCreateRequest);
+
+        userRepository.create(userCreateRequest);
+
     }
 
 }
