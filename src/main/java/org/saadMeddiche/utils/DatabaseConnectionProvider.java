1bf89c79ff2a68e2947dac4b089a0a882492d438
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-08-19 15:51:33 +0100

    feat: add HikariCP dependency for database connection pooling

--- src/main/java/org/saadMeddiche/utils/DatabaseConnectionProvider.java
+++ src/main/java/org/saadMeddiche/utils/DatabaseConnectionProvider.java
@@ -2,18 +2,32 @@ package org.saadMeddiche.utils;
 
 import com.j256.ormlite.jdbc.JdbcPooledConnectionSource;
 import com.j256.ormlite.support.ConnectionSource;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.saadMeddiche.configurations.DataBaseConfiguration;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnectionProvider {
 
     private static final DataBaseConfiguration connectionConfiguration = DataBaseConfiguration.INSTANCE;
 
+    private static final HikariDataSource ds;
+
+    static {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(connectionConfiguration.JDBC_URL);
+        config.setUsername(connectionConfiguration.JDBC_USER);
+        config.setPassword(connectionConfiguration.JDBC_PASSWORD);
+        config.addDataSourceProperty("cachePrepStmts", "true");
+        config.addDataSourceProperty("prepStmtCacheSize", "250");
+        config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
+        ds = new HikariDataSource(config);
+    }
+
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(connectionConfiguration.JDBC_URL, connectionConfiguration.JDBC_USER, connectionConfiguration.JDBC_PASSWORD);
+        return ds.getConnection();
     }
 
     public static ConnectionSource getConnectionSource() throws SQLException {
