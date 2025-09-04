42acfe75f9be53d48030cdabdb04477d79e5c0ec
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-09-04 11:41:07 +0100

    feat: add JavalinApp for setting up the Javalin server and CORS configuration

---
+++ src/main/java/org/saadMeddiche/web/JavalinApp.java
@@ -0,0 +1,48 @@
+package org.saadMeddiche.web;
+
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+import org.saadMeddiche.controllers.BookController;
+import org.saadMeddiche.controllers.ToDoController;
+
+public class JavalinApp {
+
+    private final Javalin app;
+
+    public JavalinApp() {
+        this.app = Javalin.create(config -> {
+            config.useVirtualThreads = true;
+        }).start(7070);
+    }
+
+    public void start() {
+        this.registerControllers();
+        this.cors();
+    }
+
+    private void registerControllers() {
+        new ToDoController(app);
+        new BookController(app);
+    }
+
+    private void cors() {
+        app.before(JavalinApp::corsHeaders);
+        app.options("/*", JavalinApp::corsHeadersOptions);
+    }
+
+    private static void corsHeaders(Context ctx) {
+        ctx.header("Access-Control-Allow-Origin", "http://localhost:5173");
+        ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        ctx.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        ctx.header("Access-Control-Allow-Credentials", "true");
+    }
+
+    private static void corsHeadersOptions(Context ctx) {
+        ctx.header("Access-Control-Allow-Origin", "http://localhost:5173");
+        ctx.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        ctx.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        ctx.header("Access-Control-Allow-Credentials", "true");
+        ctx.status(204);
+    }
+
+}
