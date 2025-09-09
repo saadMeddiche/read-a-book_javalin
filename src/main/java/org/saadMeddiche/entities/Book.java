b08365778ad079b0e1bf34e7bed14989b393f700
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-09-09 14:05:26 +0100

    fix: use dataType for 'coverImage' field instead of columnDefinition

--- src/main/java/org/saadMeddiche/entities/Book.java
+++ src/main/java/org/saadMeddiche/entities/Book.java
@@ -1,6 +1,7 @@
 package org.saadMeddiche.entities;
 
 import com.j256.ormlite.dao.ForeignCollection;
+import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.field.ForeignCollectionField;
 import com.j256.ormlite.table.DatabaseTable;
@@ -25,7 +26,7 @@ public class Book {
     @DatabaseField(columnDefinition = "VARCHAR(1000)")
     public String summary;
 
-    @DatabaseField(columnDefinition = "BYTEA")
+    @DatabaseField(dataType = DataType.BYTE_ARRAY)
     public byte[] coverImage;
 
     @ForeignCollectionField
