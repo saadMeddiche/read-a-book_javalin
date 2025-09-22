53d6cc088afa74e489b9b41386161f255d8ee56d
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-09-22 13:41:39 +0100

    feat: add coverImage in BookDetailsResponse

--- src/main/java/org/saadMeddiche/responses/BookDetailsResponse.java
+++ src/main/java/org/saadMeddiche/responses/BookDetailsResponse.java
@@ -9,12 +9,14 @@ public class BookDetailsResponse  {
     public String title;
     public String author;
     public String summary;
+    public byte[] coverImage;
 
     public BookDetailsResponse(ResultSet rs) throws SQLException {
         this.id = rs.getLong("id");
         this.title = rs.getString("title");
         this.author = rs.getString("author");
         this.summary = rs.getString("summary");
+        this.coverImage = rs.getBytes("cover_image");
     }
 
 }
